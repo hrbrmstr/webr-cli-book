d9d0f361b26be2a37a0adaba53a20eefbd72b38e
hrbrmstr <bob@rud.is>
2023-11-26 10:32:42 -0500

    saving

--- support/ch-08/webr-plot/index.mjs
+++ support/ch-08/webr-plot/index.mjs
@@ -1,4 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin / env node
+
+import { readFileSync } from 'fs';
+import * as path from 'path'
 import { loadPackages } from './src/webrtools.mjs';
 import { Command } from 'commander'
 import { WebR } from "webr";
@@ -17,17 +20,21 @@ program
     const webR = new WebR();
     await webR.init();
 
-    await loadPackages(webR, appDir('webr_packages'))
+    await makeAndMount(webR, appDir('pkgs'), '/pkgs')
 
     await makeAndMount(webR, cwd, '/cwd')
     await makeAndMount(webR, appDir('fonts'), '/home/web_user/fonts')
+    await makeAndMount(webR, appDir('data'), '/data')
     await makeAndMount(webR, appDir('scripts'), '/scripts')
     await makeAndMount(webR, appDir('plots'), '/plots')
 
-    await webR.evalRVoid(`
-source("/scripts/plot.R")
-`)
+    await webR.evalRVoid(`.libPaths(c("/pkgs", .libPaths()))`)
 
+    const quakesGeoJSON = await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson")
+    await webR.objs.globalEnv.bind("quakes_json", await quakesGeoJSON.text());
+
+    await webR.evalRVoid(`source("/scripts/plot.R")`)
+    
     process.exit(0)
 
   })
--- support/ch-08/webr-plot/scripts/plot.R
+++ support/ch-08/webr-plot/scripts/plot.R
@@ -1,22 +1,55 @@
 suppressPackageStartupMessages({
   library(ggplot2)
+  library(sf)
+  library(dplyr)
 })
 
-png("/plots/mtcars.png", width = 512, height = 512, units = "px")
+quakes <- st_read(quakes_json, quiet=TRUE)
+land <- st_read("/data/ne_110m_admin_0_countries.geojson", crs = st_crs(quakes))
+
+png("/plots/quakes.png", width = 1800, height = 1200, units = "px")
 
 ggplot() +
-  geom_point(
-    data = mtcars,
-    aes(x = mpg, y = disp)
+  geom_sf(
+    data = land,
+    fill = "#c3c3c3",
+    linewidth = 0.125,
+    color = "#3c3c3c"
+  ) +
+  geom_sf(
+    data = quakes,
+    aes(size = mag, fill = mag),
+    shape = 21,
+    color = "white"
+  ) +
+  scale_fill_viridis_c(
+    name = "Magnitude",
+    option = "magma"
+  ) +
+  scale_size_continuous(
+    name = "Magnitude",
+    trans = "sqrt"    
+  ) +
+  coord_sf(
+    crs = "+proj=vandg4"
+  ) +
+  guides(
+    size = guide_legend(override.aes = list(color = "black"))
   ) +
   labs(
-    title = "Inter Via JS"
+    title = "Recent Quakes",
+    subtitle = "Magnitude of 2.5 or higher that have been located by the USGS and contributing agencies within the last day",
+    caption = sprintf("Source: <https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson>\nData download: %s", Sys.time())
   ) +
   theme_minimal(
-    base_family = "Inter"
+    base_family = "Quakes"
   ) +
   theme(
-    plot.title = element_text(size = 48, face = "bold")
+    panel.background = element_rect(fill = "skyblue", color = "skyblue"),
+    plot.title = element_text(family = "Earthquake MF", size = 28, face = "bold", margin = margin(b=10)),
+    plot.subtitle = element_text(size = 20, face = "plain", margin = margin(b=15)),
+    plot.caption = element_text(margin = margin(t=10)),
+    plot.margin = margin(30, 30, 30, 30)
   ) -> gg
 
 plot(gg)
